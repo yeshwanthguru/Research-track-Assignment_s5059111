c1292f80e7262cd778873a05a5175c5fe1b7b343
yeshwanthguru <72270080+yeshwanthguru@users.noreply.github.com>
2021-04-06 05:19:22 +0000

    Update assignment1.cpp

--- assignment1/src/assignment1.cpp
+++ assignment1/src/assignment1.cpp
@@ -3,60 +3,64 @@
 #include "nav_msgs/Odometry.h" 
 #include <sstream>
 #include <iostream>
-#include "my_srv2/Random.h"
 #include "assignment1/Vel.h"
+#include "my_srv2/Random.h"
+
 double target_x = 0,target_y = 0;
-/////to declare the publisher as global/////
+
+/// declare the publisher as global///
 ros::Publisher chatter_pub;
 ros::ServiceClient client;
 ros::Publisher pub2;
-void subscriberCallback(const nav_msgs::Odometry::ConstPtr& pose_msg) ///// the information of Odometry/////
+
+void subscriberCallback(const nav_msgs::Odometry::ConstPtr& pose_msg) //  the info of Odometry///
 {
    ROS_INFO("Robot position: [%f, %f]", pose_msg->pose.pose.position.x, pose_msg->pose.pose.position.y);
-   /////To Use the pose_msg to get the current position of Odometry/////
+   /// Use the pose_msg to get the current position of Odom ///
    geometry_msgs::Twist vel;
-   ///// create the instance to call of service /////
+   /// create the instance to call service ///
    my_srv2::Random get_target;
-   ///// create the variables calculating the distance each of x and y/////
+   /// create the variables calculating the distance each of x and y///
    double dx = target_x - pose_msg->pose.pose.position.x;
    double dy = target_y - pose_msg->pose.pose.position.y;
-   ///// if the distance between target and current position is less than 0.1,change the target/////
+   /// if the distance between target and current position is less than 0.1,change the target///
    if (pow(dx,2)+ pow(dy,2) <= pow(0.1,2)) {
-     ///// set the range of random value /////
+     /// set the range of random value///
      get_target.request.min = -6;
      get_target.request.max = 6;
-     ///// call service to get the new random target /////
+     /// call service to get the new random target///
      client.call(get_target);
      target_x = get_target.response.x;
      target_y = get_target.response.y;
-     ///// calculate again to update the distance each of x and y  /////
+     /// calculate again to update the distance each of x and y///
      dx = target_x - pose_msg->pose.pose.position.x;
      dy = target_y - pose_msg->pose.pose.position.y;
    }
-    ///// calculate the velocity  /////
+    /// calculate velocity///
     double k = 1;
     vel.linear.x = k * dx; 
     vel.linear.y = k * dy; 
-    ///// publish the velocity info to stageros /////
+    /// publish the velocity info to stageros///
     chatter_pub.publish(vel);
-    ///// publishing the info of velocity as message /////
+    /// publishing the info of velocity as message///
     assignment1::Vel mymsg;
     mymsg.name = "linear"; 
     mymsg.vx = vel.linear.x;
     mymsg.vy = vel.linear.y;
     pub2.publish(mymsg);
 }
+
 int main(int argc, char **argv)
 {
-  ///// this is  for initialization /////
+  ///initialization///
   ros::init(argc, argv, "assignment1");
   ros::NodeHandle n;
-  ///// set clients to-------connect------- the  service of random_target /////
+  /// set clients to connect service of random_target_creater ///
   client = n.serviceClient<my_srv2::Random>("/random_target");
-  ///// set publisher for sending-----info------of velocity to the stageros /////
+  /// set publisher for sending info of velocity to /stageros///
   chatter_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
   pub2 = n.advertise<assignment1::Vel>("linear", 1000);
-  ///// set subscriber for getting current position from /stageros  /////
+  /// set subscriber for getting current position from /stageros///
   ros::Subscriber pose_sub = n.subscribe("odom", 1000, subscriberCallback);
   ros::spin();
     return 0;
